536dc7581647c6823245838a2309f94e37b0ef2b
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-11-14 00:57:08 +0100

    Added parallax background.

--- lib/game/cat_game.dart
+++ lib/game/cat_game.dart
@@ -2,6 +2,7 @@ import 'package:flame/components/animation_component.dart';
 import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game.dart';
 import 'package:flame/spritesheet.dart';
+import 'package:flutter/material.dart';
 
 /// CatGame class extends from BaseGame class.
 class CatGame extends BaseGame {
@@ -24,11 +25,30 @@ class CatGame extends BaseGame {
         _catSpriteSheet.createAnimation(4, from: 0, to: 7, stepTime: 0.1);
     _cat.animation = _runAnimation;
 
-    _cat.width = 200;
-    _cat.height = 200;
-    _cat.x = 100;
-    _cat.y = 100;
+    _parallaxComponent = ParallaxComponent(
+      [
+        ParallaxImage('parallax/7.png'),
+        ParallaxImage('parallax/6.png'),
+        ParallaxImage('parallax/5.png'),
+        ParallaxImage('parallax/4.png'),
+        ParallaxImage('parallax/3.png'),
+        ParallaxImage('parallax/2.png'),
+        ParallaxImage('parallax/1.png'),
+      ],
+      baseSpeed: const Offset(100, 0),
+      layerDelta: const Offset(20, 0),
+    );
 
+    /// Adding a new component to the components list.
+    add(_parallaxComponent);
     add(_cat);
+  }
+
+  @override
+  void resize(Size size) {
+    _cat.height = _cat.width = size.width / 10;
+    _cat.x = _cat.width;
+    _cat.y = size.height - 30 - _cat.height;
+    super.resize(size);
   }
 }
