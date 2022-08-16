82010070856787fad524247f8645d5018349b927
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-16 13:47:43 +0300

    Refactored enemy class.

--- lib/components/enemy.dart
+++ lib/components/enemy.dart
@@ -1,51 +1,48 @@
 import 'package:cat_game/constants/game_asset.dart';
-import 'package:flame/animation.dart';
-import 'package:flame/components/animation_component.dart';
-import 'package:flame/spritesheet.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
 
-class Enemy extends AnimationComponent {
-  SpriteSheet _catSpriteSheet;
-  Animation _runAnimation;
-  int _speed;
-  Size _size;
-
-  Enemy() : super.empty() {
+class Enemy extends SpriteAnimationComponent {
+  Enemy() : super() {
     _catSpriteSheet = SpriteSheet(
-      imageName: GameAsset.hyenaImage,
-      textureWidth: 48,
-      textureHeight: 48,
-      columns: 6,
-      rows: 1,
+      image: Flame.images.fromCache(GameAsset.hyenaImage),
+      srcSize: Vector2(48, 48),
     );
 
     _runAnimation =
-        _catSpriteSheet.createAnimation(0, from: 0, to: 5, stepTime: 0.1);
+        _catSpriteSheet.createAnimation(row: 0, to: 5, stepTime: 0.1);
+
     _speed = 200;
     run();
   }
 
-  /// This method is called periodically by the game engine
-  /// to request that your component updates itself.
+  late SpriteSheet _catSpriteSheet;
+  late SpriteAnimation _runAnimation;
+  late int _speed;
+  late Vector2 _size;
+
+  // This method is called periodically by the game engine
+  // to request that your component updates itself.
   @override
-  void update(double t) {
-    x -= _speed * t;
+  void update(double dt) {
+    x -= _speed * dt;
 
     if (x < -width) {
-      x = _size.width + width;
+      x = _size.x + width;
     }
-    super.update(t);
+    super.update(dt);
   }
 
-  /// This is a hook called by [BaseGame] to let this component
-  /// know that the screen (or flame draw area) has been update.
+  // This is a hook called by FlameGame to let this component
+  // know that the screen (or flame draw area) has been update.
   @override
-  void resize(Size size) {
-    height = width = size.width / 10;
-    x = size.width + width;
-    y = size.height - 30 - height;
+  void onGameResize(Vector2 size) {
+    height = width = size.x / 10;
+    x = size.x + width;
+    y = size.y - 30 - height;
     _size = size;
-    super.resize(size);
+    super.onGameResize(size);
   }
 
   void run() {
