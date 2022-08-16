49967eccaf948e5bafb4835591c41ddd440d0efc
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-16 13:48:24 +0300

    Refactored CatGame class.

--- lib/game/cat_game.dart
+++ lib/game/cat_game.dart
@@ -1,40 +1,44 @@
 import 'package:cat_game/components/cat.dart';
 import 'package:cat_game/components/enemy.dart';
 import 'package:cat_game/constants/game_asset.dart';
-import 'package:flame/components/parallax_component.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flame/gestures.dart';
-import 'package:flutter/material.dart';
+import 'package:flame/parallax.dart';
 
 /// CatGame class extends from BaseGame class.
-class CatGame extends BaseGame with TapDetector {
-  Cat _cat;
-  Enemy _enemy;
-  ParallaxComponent _parallaxComponent;
+class CatGame extends FlameGame with TapDetector {
+  late ParallaxComponent _parallaxComponent;
+  late Cat _cat;
+  late Enemy _enemy;
 
-  CatGame() {
+  @override
+  Future<void> onLoad() async {
+    final parallax = await getParallax();
+
+    await images.load(GameAsset.catImage);
+    await images.load(GameAsset.hyenaImage);
+
     _parallaxComponent = ParallaxComponent(
-      getParallaxImages(),
-      baseSpeed: const Offset(100, 0),
-      layerDelta: const Offset(20, 0),
+      parallax: parallax,
     );
-
-    /// Adding a new component to the components list.
-    add(_parallaxComponent);
-
     _cat = Cat();
-
-    /// Adding a new component to the components list.
-    add(_cat);
-
     _enemy = Enemy();
 
-    /// Adding a new component to the components list.
+    // Adding a new component to the components list.
+    add(_parallaxComponent);
+
+    // Adding a new component to the components list.
+    add(_cat);
+
+    // Adding a new component to the components list.
     add(_enemy);
+
+    return super.onLoad();
   }
 
   @override
-  void onTapDown(TapDownDetails details) {
+  void onTapDown(TapDownInfo details) {
     _cat.jump();
     super.onTapDown(details);
   }
@@ -50,11 +54,19 @@ class CatGame extends BaseGame with TapDetector {
   }
 
   /// Getting background images.
-  List<ParallaxImage> getParallaxImages() {
-    final List<ParallaxImage> images = <ParallaxImage>[];
-    for (String image in GameAsset.parallaxBackground) {
-      images.add(ParallaxImage(image));
+  Future<Parallax> getParallax() async {
+    final images = <ParallaxImageData>[];
+
+    for (final image in GameAsset.parallaxBackground) {
+      images.add(ParallaxImageData(image));
     }
-    return images;
+
+    final parallax = await loadParallax(
+      images,
+      baseVelocity: Vector2(100, 0),
+      velocityMultiplierDelta: Vector2(1, 0),
+    );
+
+    return parallax;
   }
 }
